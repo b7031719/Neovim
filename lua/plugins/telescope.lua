08006f004685cd243d12174fbe833fa3ecabafc7
Dan <danfenton@pm.me>
2025-08-01 21:01:06 +0100

    added more telescope picker keybindings

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -11,7 +11,9 @@ return {
 			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files"})
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
       vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
-      vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "Telescope help tags" })
+      vim.keymap.set('n', '<leader>fo', builtin.oldfiles, { desc = "Telescope old files" })
+      vim.keymap.set('n', '<leader>fr', builtin.registers, { desc = "Telescope registers" })
 		end,
 	},
 	{
