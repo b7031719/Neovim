31ed65e0975d21a416263d13cc43f2618ad15578
Dan <danfenton@pm.me>
2025-08-14 21:17:45 +0100

    added image.nvim to neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -6,8 +6,8 @@ return {
 		"nvim-lua/plenary.nvim",
 		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
 		"MunifTanjim/nui.nvim",
-		-- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
-	},
+		{"3rd/image.nvim", opts = {}},
+  },
 	lazy = true, -- neo-tree will lazily load itself
 	---@module "neo-tree"
 	---@type neotree.Config?
