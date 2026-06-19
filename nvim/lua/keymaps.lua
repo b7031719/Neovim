45d94b7b02669b5fc7c017b0d10496589ea9f9cd
b7 <danfenton@pm.me>
2026-06-19 22:36:57 +0100

    Added window move keybinds

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,2 +1,6 @@
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "moves lines down in visual selection" })
 vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv", { desc = "moves lines up in visual selection" })
+vim.keymap.set("n", "<C-h>", ":wincmd h<CR>", { desc = "move to left window" })
+vim.keymap.set("n", "<C-j>", ":wincmd j<CR>", { desc = "move to bottom window" })
+vim.keymap.set("n", "<C-k>", ":wincmd k<CR>", { desc = "move to top window" })
+vim.keymap.set("n", "<C-l>", ":wincmd l<CR>", { desc = "move to right window" })
