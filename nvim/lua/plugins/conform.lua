cd6575bdf0dea6b9952c97626e433348cba5b87d
b7 <danfenton@pm.me>
2026-06-20 18:53:22 +0100

    Adde conform.nvim for formatters

---
+++ nvim/lua/plugins/conform.lua
@@ -0,0 +1,28 @@
+return {
+	"stevearc/conform.nvim",
+	keys = {
+		{
+			"<leader>gf",
+			function()
+				require("conform").format({ lsp_fallback = true, async = false })
+			end,
+			mode = "",
+			desc = "Format buffer",
+		},
+	},
+	opts = {
+		formatters_by_ft = {
+			python = { "isort", "black" },
+			lua = { "stylua" },
+			nix = { "nixfmt" },
+			html = { "prettier" },
+			javascript = { "prettier" },
+		},
+
+		formatters = {
+			black = {
+				prepend_args = { "--line-length", "88" }, -- or your preference
+			},
+		},
+	},
+}
