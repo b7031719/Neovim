0afdc64a3514ca6bc7b5444b93b1c73f04fa740e
b7 <danfenton@pm.me>
2026-06-20 19:08:05 +0100

    Updated lsp configuration

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,90 +1,92 @@
 -- All language servers are configured in this file
 return {
-  {
-    -- Neovim built in LSP client
-    -- Sends and receives events from Neovim to language servers via language server protocol
-    "neovim/nvim-lspconfig",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	{
+		-- Neovim built in LSP client
+		-- Sends and receives events from Neovim to language servers via language server protocol
+		"neovim/nvim-lspconfig",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			local capabilities = vim.lsp.protocol.make_client_capabilities()
+			capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+			local servers = {
+				"lua_ls",
+				"pyright",
+				"nixd",
+				"ts_ls",
+				"bashls",
+				"html",
+				"cssls",
+			}
 
-      vim.lsp.config("lua_ls", {
-        capabilities = capabilities,
-        settings = {
-          Lua = {
-            diagnostics = {
-              globals = { "vim" },
-            },
-          },
-        },
-      })
-      vim.lsp.enable("lua_ls")
+      -- Global LSP config
+			vim.lsp.config("*", { capabilities = capabilities })
 
-      vim.lsp.config("pyright", {
-        capabilities = capabilities,
-      })
-      vim.lsp.enable("pyright")
+      -- lua_ls config
+			vim.lsp.config("lua_ls", {
+				settings = {
+					Lua = {
+						runtime = {
+							version = "LuaJIT",
+						},
+						diagnostics = {
+							globals = { "vim" },
+						},
+						workspace = {
+							library = { vim.env.VIMRUNTIME },
+							checkThirdParty = false,
+							preloadFileSize = 10000,
+						},
+						telemetry = { enable = false },
+						hint = { enable = true }, -- inlay hints
+					},
+				},
+			})
 
-      vim.lsp.config("nixd", {
-        capabilities = capabilities,
-        cmd = { 'nixd' },
-        settings = {
-          nixd = {
-            nixpkgs = {
-              expr = "import <nixpkgs> { }",
-            },
-            formatting = {
-              command = { "nixfmt" },
-            },
-          },
-        },
-      })
-      vim.lsp.enable("nixd")
+      -- nixd config
+			vim.lsp.config("nixd", {
+				cmd = { "nixd" },
+				settings = {
+					nixd = {
+						nixpkgs = {
+							expr = "import <nixpkgs> { }",
+						},
+						formatting = {
+							command = { "nixfmt" },
+						},
+					},
+				},
+			})
 
-      vim.lsp.config("ts_ls", {
-        capabilities = capabilities,
-      })
-      vim.lsp.enable("ts_ls")
+			-- Enable servers
+			vim.lsp.enable(servers)
 
-      vim.lsp.config("bashls", {
-        capabilities = capabilities,
-      })
-      vim.lsp.enable("bashls")
+			-- Diagnostic settings
+			vim.diagnostic.config({
+				virtual_text = true,
+				signs = true,
+				underline = true,
+				update_in_insert = false,
+				severity_sort = true,
+			})
 
-      vim.lsp.config("html", {
-        capabilities = capabilities,
-      })
-      vim.lsp.enable("html")
-
-      vim.lsp.config("cssls", {
-        capabilities = capabilities,
-      })
-      vim.lsp.enable("cssls")
-
-      -- Diagnostic settings
-      vim.diagnostic.config({
-        virtual_text = true,
-        signs = true,
-        underline = true,
-        update_in_insert = false,
-        severity_sort = true,
-      })
-
-      -- keymaps for language server functions
-      vim.keymap.set('n', 'gl', vim.diagnostic.open_float, {})
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
-      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
-      vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
-      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
-      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-      vim.keymap.set("n", "<leader>oi", function()
-        vim.lsp.buf.code_action({
-          context = { only = { "source.organizeImports" } },
-          apply = true,
-        })
-      end)
-    end,
-  },
+			-- keymaps for language server functions
+			vim.keymap.set("n", "gl", vim.diagnostic.open_float, {})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
+			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "<leader>oi", function()
+				vim.lsp.buf.code_action({
+					context = {
+						only = { "source.organizeImports" },
+						diagnostics = {}, -- helps some servers
+					},
+					apply = true,
+				})
+			end, { desc = "Organize Imports", buffer = true })
+		end,
+	},
 }
